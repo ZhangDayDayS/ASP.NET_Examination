3eaafe3dab64f01531d8450ab7109e2840a965ec
ZhangDayDayS <54056837+ZhangDayDayS@users.noreply.github.com>
2020-08-09 15:35:18 +0800

    Add files via upload

---
+++ DBHelper.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Shopping.DAL
+{
+    public class DBHelper
+    {
+
+        string constr = @"Data Source=ZhangDayS;Initial Catalog=Shopping;Integrated Security=True";
+        //增删改(简单查询)
+        public int Update(string sql)
+        {
+            int rows = 0;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    rows = cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+
+                }
+            }
+            return rows;
+        }
+        //增删改(参数化查询)
+        public int Update(string sql, SqlParameter[] param, CommandType type)
+        {
+            int rows = 0;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.CommandType = type;
+                    cmd.Parameters.AddRange(param);
+                    rows = cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+
+                }
+            }
+            return rows;
+        }
+        //查询单个数据(简单查询)
+        public object QueryOne(string sql)
+        {
+            object obj = null;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    obj = cmd.ExecuteScalar();
+                }
+                catch
+                {
+
+                }
+            }
+            return obj;
+        }
+        //查询单个数据(参数化查询)
+        public object QueryOne(string sql, SqlParameter[] param, CommandType type)
+        {
+            object obj = null;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.CommandType = type;
+                    cmd.Parameters.AddRange(param);
+                    obj = cmd.ExecuteScalar();
+                }
+                catch
+                {
+
+                }
+            }
+            return obj;
+        }
+        //使用SqlDataReader查询多行多列数据(简单查询)
+        public SqlDataReader QuerySet(string sql)
+        {
+            SqlDataReader reader = null;
+            SqlConnection con = new SqlConnection(constr);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                //throw ex;
+                con.Close();
+            }
+            return reader;
+        }
+        //使用SqlDataReader查询多行多列数据(参数化查询)
+        public SqlDataReader QuerySet(string sql, SqlParameter[] param, CommandType type)
+        {
+            SqlDataReader reader = null;
+            SqlConnection con = new SqlConnection(constr);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = type;
+                cmd.Parameters.AddRange(param);
+                reader = cmd.ExecuteReader();
+            }
+            catch
+            {
+                con.Close();
+            }
+            return reader;
+        }
+        //使用DataSet查询多行多列数据(简单查询)
+        public DataSet GetData(string sql)
+        {
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                sda.Fill(ds);
+            }
+            catch
+            {
+
+            }
+            return ds;
+        }
+        //使用DataSet查询多行多列数据(参数化查询)
+        public DataSet GetData(string sql, SqlParameter[] param, CommandType type)
+        {
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = type;
+            cmd.Parameters.AddRange(param);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                sda.Fill(ds);
+            }
+            catch
+            {
+
+            }
+            return ds;
+        }
+    }
+}
---
+++ Examination.sql
@@ -0,0 +1,47 @@
+Use Master
+Go
+if exists(Select * From sys.databases Where Name = 'Examination')
+Drop DataBase Examination
+Go
+Create Database Examination
+On
+(
+	name = 'Examination_data',
+	filename = 'C:\SQL\Examination_data.mdf'
+)
+Log on
+(
+	name = 'Examination_log',
+	filename = 'C:\SQL\Examination_log.ldf'
+)
+Go
+Use Examination
+Go
+If Exists(Select * From sys.tables Where Name = 'Grade')
+Drop Table Grade
+Go
+Create Table Grade
+(
+	Id Int Primary Key Identity(1,1) Not Null,
+	GradeName Varchar(50) Not Null
+)
+Go
+If Exists(Select * From sys.tables Where Name = 'Book')
+Drop Table Book
+Go
+Create Table book
+(
+	Id int Primary Key Identity(1,1) Not Null,
+	BookName Varchar(50) Unique Not Null,
+	Grade_Id Int References Grade(Id) Not Null
+)
+Go
+Insert Into Grade Values('S1'),('S2'),('Y2')
+Insert Into Book Values('C#',1),('Net',2),('HTML',3)
+Go
+Select * From Grade
+Select * From Book
+Select * From Grade Inner Join Book on Grade.Id = Book.Id
+-- Delete From book where Grade_Id = 2
+
+-- insert into Book values('C++',1)
